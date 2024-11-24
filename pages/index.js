68308490f96cdda2c23d24a7163cb509a9c2bda6
Serafin Patino <serafinpatino@Serafins-MacBook-Pro.local>
2024-11-23 20:54:09 -0600

    adding random text to test ngrok

--- pages/index.js
+++ pages/index.js
@@ -99,7 +99,7 @@ export default function Home() {
                   onClick={() => deleteItem(item.id)}
                   className="ml-8 p-4 border-slate-900 hover:bg-slate-900 w-16"
                 >
-                  X
+                  X --- ADDING RANDOM TO TEST NGROK
                 </button>
               </li>
             ))}
